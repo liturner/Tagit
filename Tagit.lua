ea6b902c9654d1b9c4a368828745395b8404f37a
Luke Turner <turner.luke@outlook.com>
2019-08-10 20:18:06 +0200

    Refactored to the name "Tagit" Remove "print()" debugging

--- Tagit.lua
+++ Tagit.lua
@@ -1,33 +1,26 @@
-local name, lMiniMap = ...
+local name, Tagit = ...
 local lineAdded = false
-lMiniMap.MainFrame = CreateFrame('FRAME', nil, UIParent)
+Tagit.MainFrame = CreateFrame('FRAME', nil, UIParent)
 
-function lMiniMap.OnEvent(self, event, ...) 
-	print(event)
-	if event == "ADDON_LOADED" and ... == "lMiniMap" then
-		print('lMiniMap: ADDON_LOADED')
-		
+function Tagit.OnEvent(self, event, ...) 
+	if event == "ADDON_LOADED" and ... == "Tagit" then		
 		-- Make sure we have an initialised settings database
-		if TAPIV_Settings == nil then
-			TAPIV_Settings = { AddonName = 'Forager', NodesLootedDatabase = {} }
+		if Tagit_Database == nil then
+			Tagit_Database = { AddonName = 'Forager', NodesLootedDatabase = {} }
 		end
 		
 		-- Tie onto the "OnClick" for all bag slots
-		lMiniMap.RegisterAllBagSlotsForOnClick()		
+		Tagit.RegisterAllBagSlotsForOnClick()		
 		
 		self:UnregisterEvent("ADDON_LOADED")
 	end
-	
-	if GetMouseButtonClicked() == 'LeftButton' then
-		print("TIMMEH!")
-	end
 end
 
-local function OnTooltipSetItem(tooltip)
+function Tagit.OnTooltipSetItem(tooltip, ...)
 	if not lineAdded then
 		tooltipItem = tooltip:GetItem()
-		tooltipNoteID = TAPIV_Settings[tooltipItem]
-		tooltipNote = lMiniMap.NoteIDToText(tooltipNoteID)
+		tooltipNoteID = Tagit_Database[tooltipItem]
+		tooltipNote = Tagit.NoteIDToText(tooltipNoteID)
 		
 		if tooltipNote ~= nil then
 			tooltip:AddLine(tooltipNote)
@@ -36,28 +29,23 @@ local function OnTooltipSetItem(tooltip)
 	end
 end
 
-local function OnTooltipCleared(tooltip, ...)
+function Tagit.OnTooltipCleared(tooltip, ...)
    lineAdded = false
 end
 
 -- Update the database and trigger a UI Refresh
-function lMiniMap.OnItemSelectedForMarking(itemName)
-	if TAPIV_Settings[itemName] == nil then
-		TAPIV_Settings[itemName] = 1
-	elseif TAPIV_Settings[itemName] < 4 then
-		TAPIV_Settings[itemName] = TAPIV_Settings[itemName] + 1
+function Tagit.OnItemSelectedForMarking(itemName)
+	if Tagit_Database[itemName] == nil then
+		Tagit_Database[itemName] = 1
+	elseif Tagit_Database[itemName] < 4 then
+		Tagit_Database[itemName] = Tagit_Database[itemName] + 1
 	else
-		TAPIV_Settings[itemName] = 0
+		Tagit_Database[itemName] = 0
 	end
-
-	print("itemName" .. itemName)
-	
-	note = lMiniMap.NoteIDToText(TAPIV_Settings[itemID])
-	if note ~= nil then print("Note" .. note) end
 end
 
 -- Return nil or a readable string. Can recieve nil safely
-function lMiniMap.NoteIDToText(noteID)
+function Tagit.NoteIDToText(noteID)
 	if noteID == 0 then 
 		return nil
 	elseif noteID == 1 then
@@ -73,7 +61,7 @@ function lMiniMap.NoteIDToText(noteID)
 	end
 end
 
-function lMiniMap.RegisterAllBagSlotsForOnClick()
+function Tagit.RegisterAllBagSlotsForOnClick()
 	for b = 1, 5 do
 		for s = 0, 32 do -- using 32 as a max bag size and therefore a max count for button frame creations, could be an issue, need to know if all bag slot buttons are created on game start and just hidden OR are they created per bag equipped ?
 			-- get global name
@@ -87,7 +75,7 @@ function lMiniMap.RegisterAllBagSlotsForOnClick()
 						if GetMouseButtonClicked() == 'LeftButton' and itemID ~= nil then
 						
 							itemName = GetItemInfo(itemID)
-							lMiniMap.OnItemSelectedForMarking(itemName)
+							Tagit.OnItemSelectedForMarking(itemName)
 						end
 					end
 				end)
@@ -96,7 +84,7 @@ function lMiniMap.RegisterAllBagSlotsForOnClick()
 	end
 end
 
-lMiniMap.MainFrame:RegisterEvent('ADDON_LOADED')
-lMiniMap.MainFrame:SetScript('OnEvent', lMiniMap.OnEvent)
-GameTooltip:HookScript("OnTooltipSetItem", OnTooltipSetItem)
-GameTooltip:HookScript("OnTooltipCleared", OnTooltipCleared)
+Tagit.MainFrame:RegisterEvent('ADDON_LOADED')
+Tagit.MainFrame:SetScript('OnEvent', Tagit.OnEvent)
+GameTooltip:HookScript("OnTooltipSetItem", Tagit.OnTooltipSetItem)
+GameTooltip:HookScript("OnTooltipCleared", Tagit.OnTooltipCleared)
