2a6a2d64a8453d836cf71959bdf569dd81d16a0c
Luke Ian Turner <turnertech@outlook.de>
2023-10-20 10:32:45 +0200

    Add slash command addtag

--- Tagit.lua
+++ Tagit.lua
@@ -1,20 +1,19 @@
 local name, addon = ...
 
-Tagit = {
+TagitMixin = {
 	Debug = false,
 	name = name,
 	LineAdded = false
 }
 
-function Tagit:OnLoad()
-	Tagit.Frame = self
+function TagitMixin:OnLoad()
+	TagitMixin.Frame = self
 
 	if Tagit_Items == nil then
 		Tagit_Items = {}
 	end
 
 	if Tagit_Tags == nil then
-		-- TODO: This should be a migration. The Items part should remain untouched.
 		Tagit_Tags = { 
 			{
 				GUID = "6945e800-19fa-4a85-bbe7-265c6768aa62",
@@ -34,23 +33,51 @@ function Tagit:OnLoad()
 			}
 		}
 	end
-	
+
 	-- Tie onto the "OnClick" for all bag slots
 	self:RegisterAllBagSlotsForOnClick()
 
-	GameTooltip:HookScript("OnTooltipSetItem", Tagit.OnTooltipSetItem)
-	GameTooltip:HookScript("OnTooltipCleared", Tagit.OnTooltipCleared)
+	GameTooltip:HookScript("OnTooltipSetItem", TagitMixin.OnTooltipSetItem)
+	GameTooltip:HookScript("OnTooltipCleared", TagitMixin.OnTooltipCleared)
+
+	SLASH_ADDTAG1 = "/addtag"
+	SlashCmdList["ADDTAG"] = function(label) self:SlashNewTag(label) end
 
 	InterfaceOptions_AddCategory(self)
 end
 
-function Tagit:print(text)
+-- This is only intended for an incoming slash command. It splits by comma
+-- and passes the ars on to "NewTag"
+function TagitMixin:SlashNewTag(arg)
+	local args = {}
+	for word in string.gmatch(arg, '([^,]+)') do
+		table.insert(args, word)
+	end
+	return self:NewTag(args[1], args[2])
+end
+
+function TagitMixin:NewTag(label, id)
+	if(not label) then
+		print("ToDo: Show Error")
+		return
+	end
+
+	if(not id) then
+		-- ToDo: Need to check if the random ID already exists first...
+		-- The first 50 numbers are just reserved IDs
+		id = math.random(50, 2147483646)
+	end
+
+	table.insert(Tagit_Tags, {GUID=id, Label=label})
+end
+
+function TagitMixin:print(text)
 	if(self.Debug) then
 		print(text)
 	end
 end
 
-function Tagit:TagFromGUID(GUID)
+function TagitMixin:TagFromGUID(GUID)
 	self:print("TagFromGUID: " .. tostring(GUID))
 	if not GUID then
 		return nil
@@ -63,46 +90,69 @@ function Tagit:TagFromGUID(GUID)
 	end
 end
 
-function Tagit.OnTooltipSetItem(tooltip, ...)
-	if not Tagit.LineAdded then
+-- Delete this function once the DB has no Name keys
+function TagitMixin.CleanDb(item)
+
+	if not item then
+		return
+	end
+
+	local itemID, itemType, itemSubType, itemEquipLoc, icon, classID, subclassID = GetItemInfoInstant(item)
+	local itemName = GetItemInfo(item)
+
+	TagitMixin:print("Clean DB: " .. tostring(itemID) .. " = " .. tostring(itemName))
+
+	if(Tagit_Items[itemName]) then
+		Tagit_Items[itemID] = Tagit_Items[itemName]
+		Tagit_Items[itemName] = nil
+	end
+
+end
+
+function TagitMixin.OnTooltipSetItem(tooltip, ...)
+	if not TagitMixin.LineAdded then
 		local tooltipItem = tooltip:GetItem()
-		local tooltipNoteID = Tagit_Items[tooltipItem]
-		local tooltipNote = Tagit:NoteGUIDToText(tooltipNoteID)
+
+		TagitMixin.CleanDb(tooltipItem)
+
+		local itemID, itemType, itemSubType, itemEquipLoc, icon, classID, subclassID = GetItemInfoInstant(tooltipItem)
+		local tooltipNoteID = Tagit_Items[itemID]
+		local tooltipNote = TagitMixin:NoteGUIDToText(tooltipNoteID)
 		
 		if tooltipNote ~= nil then
 			tooltip:AddLine(tooltipNote)
-			Tagit.LineAdded = true
+			TagitMixin.LineAdded = true
 		end
 	end
 end
 
-function Tagit.OnTooltipCleared(tooltip, ...)
-	Tagit.LineAdded = false
+function TagitMixin.OnTooltipCleared(tooltip, ...)
+	TagitMixin.LineAdded = false
 end
 
 -- Update the database and trigger a UI Refresh
-function Tagit:OnItemSelectedForMarking(itemName)
-	Tagit.print("Marking: " .. itemName)
+function TagitMixin:OnItemSelectedForMarking(itemId)
+	TagitMixin.print("Marking: " .. itemId)
 
-	local currentTagId, _ = self:TagFromGUID(Tagit_Items[itemName])
+	local currentTagId, _ = self:TagFromGUID(Tagit_Items[itemId])
 
 	self:print("Marking: " .. "Current ID  = " .. tostring(currentTagId))
-	self:print("Marking: " .. "Current Tag = " .. tostring(Tagit_Items[itemName]))
+	self:print("Marking: " .. "Current Tag = " .. tostring(Tagit_Items[itemId]))
 
-	if not Tagit_Items[itemName] then
-		Tagit_Items[itemName] = Tagit_Tags[1].GUID
+	if not Tagit_Items[itemId] then
+		Tagit_Items[itemId] = Tagit_Tags[1].GUID
 	elseif currentTagId < #Tagit_Tags then
-		Tagit_Items[itemName] = Tagit_Tags[currentTagId + 1].GUID
+		Tagit_Items[itemId] = Tagit_Tags[currentTagId + 1].GUID
 	else
-		Tagit_Items[itemName] = nil
+		Tagit_Items[itemId] = nil
 	end
 
-	self:print("Marking: " .. "New Tag     = " .. tostring(Tagit_Items[itemName]))
+	self:print("Marking: " .. "New Tag     = " .. tostring(Tagit_Items[itemId]))
 
 end
 
 -- Return nil or a readable string. Can recieve nil safely
-function Tagit:NoteGUIDToText(noteGUID)
+function TagitMixin:NoteGUIDToText(noteGUID)
 	self:print("Labeling: " .. tostring(noteGUID))
 
 	local tagId, tag = self:TagFromGUID(noteGUID)
@@ -116,7 +166,7 @@ function Tagit:NoteGUIDToText(noteGUID)
 	end
 end
 
-function Tagit:RegisterAllBagSlotsForOnClick()
+function TagitMixin:RegisterAllBagSlotsForOnClick()
 	for b = 1, 5 do
 		for s = 0, 32 do -- using 32 as a max bag size and therefore a max count for button frame creations, could be an issue, need to know if all bag slot buttons are created on game start and just hidden OR are they created per bag equipped ?
 			-- get global name
@@ -128,8 +178,7 @@ function Tagit:RegisterAllBagSlotsForOnClick()
 
 						-- Trigger event if not nil
 						if GetMouseButtonClicked() == 'LeftButton' and itemID ~= nil then
-							local itemName = GetItemInfo(itemID)
-							self:OnItemSelectedForMarking(itemName)
+							self:OnItemSelectedForMarking(itemID)
 						end
 					end
 				end)
