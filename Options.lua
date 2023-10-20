e32940a189381686cf3348efbe9bc0831212125e
Luke Ian Turner <turnertech@outlook.de>
2023-10-20 21:39:10 +0200

    Further Options Frame code

--- Options.lua
+++ Options.lua
@@ -1,13 +1,30 @@
-local name, addon = ...
+local name, Addon = ...
 
 TagitOptionsMixin = {
 	name = name,
 }
 
 function TagitOptionsMixin:OnLoad()
+	self.AddKeyButton:SetScript("OnClick", function() self:OnAddClicked() end)
 	InterfaceOptions_AddCategory(self)
 end
 
-function TagitOptionsMixin:OnAddTag()
-	Tagit_Addon:NewTag("From The GUI", 12037)
+function TagitOptionsMixin:OnAddClicked()
+	if(self.AddKeyEditBox:IsTagExisting()) then
+		UIErrorsFrame:AddExternalErrorMessage("Cannot add a tag. It already exists!")
+	end
+end
+
+AddKeyEditBoxMixin = {}
+
+function AddKeyEditBoxMixin:OnTextChanged(userInput)
+	if(self:IsTagExisting()) then
+		print("Exists")
+	else
+		print("Available")
+	end
+end
+
+function AddKeyEditBoxMixin:IsTagExisting()
+	return Addon.Tags:GetTagFromTagLabel(self:GetText()) ~= nil
 end
